520798322fc71894e6c2c006aba59885bcee8d62
Jose Faustino <josefaustino.channel@gmail.com>
2022-02-10 14:15:31 -0700

    MVP

--- Conditionals/ViewController.swift
+++ Conditionals/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    
+    //Declare Variables
+    
+    var x = 3 // This variable is for our if statement.
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //If statement
+    //Checks the Boolean Condition to see if it is true or flase. If the condition is true, it will execute the code within its curly braces. If it's false, it will skip that part of the code.
 
+    
+    @IBAction func whenPress(_ sender: Any)
+    {
+        x = 5
+        //Boolean condition is x == 3. It checks to see if x is equal to 3.
+        if x == 3
+        {
+        print(x)
+            
+        }
+        else if x == 4
+        {
+            print("x is equal to 4")
+        }
+        else
+        {
+            print("x is not equal to 3 or 4")
+        }
+        switch x
+        {
+        case 0:
+            print ("x is equal to 0")
+        case 4:
+            print("x is equal to 4")
+        case 5:
+            print("x is equal to 5")
+        default:
+            print ("x is not any of the values")
+    }
+    
 }
 
+
+}
